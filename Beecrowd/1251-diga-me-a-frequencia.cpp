03940549cf10a4c4f18559ae05b0eb3b9ff0dd44
Felipe Assuncão <meframos1@gmail.com>
2026-04-14 07:31:09 -0300

    feat: adiciona arquivos do exercício cli-dice

--- Beecrowd/1251-diga-me-a-frequencia.cpp
+++ Beecrowd/1251-diga-me-a-frequencia.cpp
@@ -1,47 +1,22 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <map>
 
-struct Caractere {
-    int ascii;
-    int freq;
-};
 
-bool comparar(Caractere a, Caractere b) {
-    if (a.freq != b.freq) {
-        return a.freq < b.freq;
-    }
-    return a.ascii > b.ascii;   
-}
 
 int main() {
+
+
     std::string linha;
-    bool primeira_linha = true;
-
-    while (std::getline(std::cin, linha)) {
-        if (!primeira_linha) std::cout << std::endl;
-        primeira_linha = false;
-
-        int frequencias[256] = {0};
-        for (char c : linha) {
-            frequencias[(unsigned char)c]++;
+    
+    int freq[256];
+    while(std::getline(std::cin, linha)) {
+        int freq[256] = {0};
+        
+        for(char c : linha) {
+            freq[(usigned char)c]++;
         }
 
-        std::vector<Caractere> lista;
-        for (int i = 0; i < 256; i++) {
-            if (frequencias[i] > 0) {
-                lista.push_back({i, frequencias[i]});
-            }
-        }
-
-        std::sort(lista.begin(), lista.end(), comparar);
-
-        for (const auto& item : lista) {
-            std::cout << item.ascii << " " << item.freq << std::endl;
-        }
+        
+        std::cout << std::endl;
     }
-
-    return 0;
 }
